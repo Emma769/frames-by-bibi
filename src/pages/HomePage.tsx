e4c5d72afaa91371404eb35419c75494e3bc74bc
huey <emma769ose@gmail.com>
2025-07-03 03:09:27 +0100

    made a small adjustment to hero bg image

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
     <div className="min-h-[200dvh]">
-      <section className="h-[calc(100dvh-6.5em)] bg-[url('/lensabl-LEtgz-OS5uU-unsplash.jpg')] bg-fixed bg-cover relative grid place-content-center after:absolute after:content-[''] after:inset-0 after:bg-[rgba(255,102,0,.2)]">
+      <section className="h-[calc(100dvh-6.5em)] bg-[url('/lensabl-LEtgz-OS5uU-unsplash.jpg')] bg-no-repeat bg-center bg-fixed bg-cover relative grid place-content-center after:absolute after:content-[''] after:inset-0 after:bg-[rgba(255,102,0,.2)]">
         <Link
           to="/catalog"
           className="text-center text-4xl text-[#fff] relative z-[1]"
