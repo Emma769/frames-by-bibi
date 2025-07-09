b2d7802abc6c23595155b11cff102d7f91c2984b
huey <emma769ose@gmail.com>
2025-07-09 23:07:11 +0100

    made another small style adjustment

--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../ui/Navbar";
+import Footer from "../ui/Footer";
 
 export default function BaseLayout() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   );
 }
