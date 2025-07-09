cf59cc5ce8e7a32eceb9f5f68eb2b3f1dd3c55c0
huey <emma769ose@gmail.com>
2025-07-09 16:53:25 +0100

    add collection section to the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,6 +8,7 @@ export default function HomePage() {
     <main>
       <HeroSection />
       <FeaturedProducts />
+      <ProductCollection />
     </main>
   );
 }
@@ -76,7 +77,7 @@ function FeaturedProducts() {
     useHorizontalScroll<HTMLDivElement>(1);
 
   return (
-    <section className="py-8 bg-[hsl(24,100%,97%)]">
+    <section className="py-10 bg-[hsl(24,100%,97%)]">
       <div>
         <p className="text-center font-[bebas] font-semibold tracking-wider text-2xl">
           <span className="bg-[hsl(24,100%,75%)] py-1 px-2 -rotate-3 inline-block">
@@ -118,5 +119,61 @@ function FeaturedProducts() {
         </Container>
       </div>
     </section>
+  );
+}
+
+function ProductCollection() {
+  return (
+    <div className="py-8 bg-[hsl(24,100%,75%)] min-h-fit">
+      <CrossAnimation />
+      <Container>
+        <div className="flex w-full flex-col sm:flex-row h-full px-5 py-16 gap-5">
+          <Link
+            to="/"
+            className="block w-full h-[25em] sm:h-[28em] md:h-[32em] relative after:content-[''] after:absolute after:bg-[rgba(255,102,0,.1)] after:inset-0"
+          >
+            <img
+              src="/toa-heftiba-GnkfcQxuj7A-unsplash.jpg"
+              alt=""
+              className="block w-full h-full object-cover"
+            />
+            <p className="absolute text-nowrap top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 font-[bebas] bg-[hsl(24,100%,97%)] text-[#ff6600] font-bold text-2xl p-2 tracking-wide -rotate-3">
+              shop baby items
+            </p>
+          </Link>
+          <Link
+            to="/"
+            className="block w-full h-[25em] sm:h-[28em] md:h-[32em] relative after:content-[''] after:absolute after:bg-[rgba(255,102,0,.1)] after:inset-0"
+          >
+            <img
+              src="nathan-dumlao-VJHb4QPBgV4-unsplash.jpg"
+              alt=""
+              className="block w-full h-full object-cover"
+            />
+            <p className="absolute text-nowrap top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 font-[bebas] bg-[hsl(24,100%,97%)] text-[#ff6600] font-bold text-2xl p-2 tracking-wide -rotate-3">
+              shop eyewear
+            </p>
+          </Link>
+        </div>
+      </Container>
+      <CrossAnimation />
+    </div>
+  );
+}
+
+function CrossAnimation() {
+  return (
+    <div className="grid grid-cols-7 px-16">
+      {Array.from({ length: 7 }, (_, idx) => {
+        return (
+          <div key={`cross_${idx}`} className="cross-container">
+            <div className="cross">
+              <div className="vertical"></div>
+              <div className="horizontal"></div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
